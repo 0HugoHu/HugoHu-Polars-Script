076c06c3a37d219738bda781cdfa5d0df9a42f32
yh342 <yh342@duke.edu>
2023-09-17 14:14:04 -0400

    Week 3

--- ids706_python_template/cli.py
+++ ids706_python_template/cli.py
@@ -1,28 +1,50 @@
 """CLI interface for ids706_python_template project.
-
-Be creative! do whatever you want!
-
-- Install click or typer and create a CLI app
-- Use builtin argparse
-- Start a web application
-- Import things from your .base module
 """
 
+import pandas as pd
+import random
+import polars as pl
+import matplotlib.pyplot as plt
 
-def main():  # pragma: no cover
-    """
-    The main function executes on commands:
-    `python -m ids706_python_template` and `$ ids706_python_template `.
+def createNewCSV():
+    # Generate random data
+    data = {
+        "ID": range(1, 10001),  # IDs from 1 to 10000
+        "Name": [f"Person_{i}" for i in range(1, 10001)],
+        "Age": [random.randint(18, 60) for _ in range(1, 10001)],
+        "Salary": [random.randint(30000, 100000) for _ in range(1, 10001)],
+    }
 
-    This is your program's entry point.
+    # Create a DataFrame
+    df = pd.DataFrame(data)
 
-    You can change this function to do whatever you want.
-    Examples:
-        * Run a test suite
-        * Run a server
-        * Do some other stuff
-        * Run a command line application (Click, Typer, ArgParse)
-        * List all available tasks
-        * Run an application (Flask, FastAPI, Django, etc.)
-    """
-    print("This will do something")
+    # Save the DataFrame to a CSV file
+    df.to_csv("large_data.csv", index=False)
+
+    print("Large dataset saved to 'large_data.csv'")
+ 
+
+def visualizeData():
+    # Read the CSV file into a Polars DataFrame
+    df = pl.read_csv('large_data.csv')
+
+    # Calculate descriptive statistics
+    stats = df.describe()
+
+    # Print the statistics
+    print(stats)
+
+    # Visualize the data (e.g., a histogram of ages)
+    df.select('Salary').to_pandas().plot(kind='hist', bins=10, rwidth=0.9)
+    plt.xlabel('Salary')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Salary')
+    plt.show()
+    plt.savefig('plot.png')
+
+
+def main():
+
+    createNewCSV()
+
+    visualizeData()
